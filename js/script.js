02c6768a5d4f86ae4bc39ea3331282e5c7d1f5e5
Andrey <kotlyar.andrey03@mail.ru>
2024-07-11 19:16:40 +0300

    Тренировка классовых компонентов

--- js/script.js
+++ js/script.js
@@ -149,7 +149,53 @@ window.addEventListener('DOMContentLoaded', () => {
             window.removeEventListener('scroll', showModalByScroll);
         }
     };
-
+    
     window.addEventListener('scroll', showModalByScroll);
     
+
+    //ИСПОЛЬЗУЕМ КЛАССЫ ДЛЯ КАРТОЧЕК МЕНЮ
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 89;
+            this.changeToRub();
+        }
+    //Конвертация цены в рубли
+        changeToRub() {
+            return this.price * this.transfer;
+        }
+    //Динамически создаем карточки
+        render(){
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                    </div>
+                </div>
+            `;
+    //Помещаем элемент на страницу
+            this.parent.append(element);
+        };
+    }
+    //Заполнение карточки
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        229,
+        '.menu.container'
+    ).render();
+
 });
